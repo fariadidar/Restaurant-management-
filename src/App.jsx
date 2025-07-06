f1cf594cb9da29306b6245aaf84e362ca510cab5
fariadidar <168257621+fariadidar@users.noreply.github.com>
2025-07-06 23:20:04 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+//import reactLogo from './assets/react.svg'
+import './App.css'
+import Header from './Components/Shared/Header'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <h1>Hello</h1>
+    </>
+  )
+}
+
+export default App
